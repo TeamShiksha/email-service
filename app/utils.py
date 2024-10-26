354ea6d8a7bfa1c07c0565d573640a5417f52ca8
Anoop Pratap Singh <anoopprsingh.social@gmail.com>
2024-10-26 20:07:03 +0530

    basic working app (#2)

---
+++ app/utils.py
@@ -0,0 +1,8 @@
+"""
+For third party interation and custom helper function.
+"""
+
+# add function to generate custom response which can be used by response model
+
+
+# add emailsending object here.
