58f618d77e2a758048dadea067c0bc6a4dfec9b1
aps08 <anoopprsingh.social@gmail.com>
2025-03-05 01:43:47 +0530

    import change for vercel deploy

--- run.py
+++ run.py
@@ -6,8 +6,12 @@ Start of the Application
 import uvicorn
 from dotenv import load_dotenv
 from app.config import config
+from app.main import app
 
 load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", port=config.PORT, reload=True)
+    # uvicorn.run(app, port=config.PORT) --> to run on vercel
+    # uvicorn.run("app.main:app", port=config.PORT, reload=True)
+    # --> run locally with auto reload on change
+    uvicorn.run(app, port=config.PORT)
