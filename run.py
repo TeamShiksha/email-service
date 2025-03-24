fa44830b76e4fd6eb3033c2f3675f0410e354621
aps08 <anoopprsingh.social@gmail.com>
2025-03-24 22:23:47 +0530

    deploy

--- run.py
+++ run.py
@@ -11,6 +11,6 @@ from app.main import app
 load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", port=config.PORT, reload=True)
+    # uvicorn.run("app.main:app", port=config.PORT, reload=True)
     # --> run locally with auto reload on change
-    # uvicorn.run(app, port=config.PORT) # to run on vercel
+    uvicorn.run(app, port=config.PORT) # to run on vercel
