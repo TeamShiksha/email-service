1f2d2af0f3470fc4ef43c863f6f83e03f925ff52
Anoop Pratap Singh <anoopprsingh.social@gmail.com>
2025-04-07 22:56:11 +0530

    Revert "SES integration (#23)" (#24)
    This reverts commit 0289040406fa6668d4c60951fd4a4d361c1d126f.

--- app/routers/email_router.py
+++ app/routers/email_router.py
@@ -2,16 +2,12 @@
 Email router for creating and controlling the endpoint. 
 """
 
-from typing import Dict, Union
-
 from fastapi import APIRouter, Depends, HTTPException, status, Request
 from fastapi.templating import Jinja2Templates
-
 from app.services.email_service import EmailService
-from app.schemas.email import SendEmailRequestBody, SendEmailResponseBody, EmailProvider
+from app.schemas.email import SendEmailRequestBody, SendEmailResponseBody
 from app.utils import (
     EmailSender,
-    SESEmailSender,
     success_response,
     get_email_sender,
     require_authentication,
@@ -27,9 +23,7 @@ templates = Jinja2Templates(directory="templates")
 async def send_email(
     request: Request,
     email_details: SendEmailRequestBody,
-    email_sender: Dict[EmailProvider, Union[EmailSender, SESEmailSender]] = Depends(
-        get_email_sender
-    ),
+    email_sender: EmailSender = Depends(get_email_sender),
 ):
     """
     Sends an email to the recipient using provided details.
--- app/services/email_service.py
+++ app/services/email_service.py
@@ -2,13 +2,9 @@
 Third party email service configuration for sending emails.
 """
 
-from typing import Dict, Union
-
 from smtplib import SMTPException, SMTPAuthenticationError, SMTPSenderRefused
-import botocore.exceptions
-
-from app.utils import EmailSender, SESEmailSender
-from app.schemas.email import SendEmailRequestBody, EmailProvider
+from app.utils import EmailSender
+from app.schemas.email import SendEmailRequestBody
 
 
 class EmailService:
@@ -16,48 +12,35 @@ class EmailService:
     Service layer for handling email sending logic.
     """
 
-    def __init__(
-        self, email_sender: Dict[EmailProvider, Union[EmailSender, SESEmailSender]]
-    ):
+    def __init__(self, email_sender: EmailSender):
         self.email_sender = email_sender
 
     def send_email(self, email_details: SendEmailRequestBody, body: str):
         """
         Sends an email based on email details.
+
         Args:
             email_details (SendEmailRequestBody): Details for the email.
             body (str): body of the email.
 
         Returns:
-            dict: A response dictionary containing:
-                - "success" (bool): Indicates whether the email was sent successfully.
-                - "error": Error message if the email sending fails.
+            bool: True if no failure occured on sending email.
         """
         try:
-            result = self._send_email_by_provider(email_details, body)
+            result = self.email_sender.send_email(
+                to_email=email_details.recipient,
+                subject=email_details.subject,
+                body=body,
+                cc=email_details.cc,
+                bcc=email_details.bcc,
+                is_html=True,
+            )
             return result
-        except Exception as error:
-            self._handle_email_error(error)
-
-    def _send_email_by_provider(self, email_details: SendEmailRequestBody, body: str):
-        provider = email_details.provider
-        sender = self.email_sender.get(provider)
-        result = sender.send_email(
-            to_email=email_details.recipient,
-            subject=email_details.subject,
-            body=body,
-            cc=email_details.cc,
-            bcc=email_details.bcc,
-            is_html=True,
-        )
-        return result
-
-    def _handle_email_error(self, error: Exception):
-        if isinstance(
-            error, (SMTPAuthenticationError, botocore.exceptions.ClientError)
-        ):
-            raise PermissionError(f"Authentication failed: {str(error)}") from error
-        elif isinstance(error, SMTPSenderRefused):
-            raise ValueError(f"Email address refused: {str(error)}") from error
-        else:
-            raise ConnectionError(f"Email sending failed: {str(error)}") from error
+        except SMTPAuthenticationError as exc:
+            raise PermissionError(
+                "Failed to authenticate with the SMTP server."
+            ) from exc
+        except SMTPSenderRefused as exc:
+            raise ValueError("Sender address refused by the SMTP server.") from exc
+        except SMTPException as smtp_exc:
+            raise ConnectionError(f"SMTP error occurred: {smtp_exc}") from smtp_exc
