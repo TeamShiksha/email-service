bf6b6f6b2e8e664f35cec483bce1084c2f904051
Sasidharan Senthilkumar <sasidharan192000@gmail.com>
2025-04-09 21:47:54 +0530

    Reapply "SES integration (#23)" (#24)
    This reverts commit 1f2d2af0f3470fc4ef43c863f6f83e03f925ff52.
    
    Reapply "SES integration (#23)"

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -2,9 +2,13 @@
 Third party email service configuration for sending emails.
 """
 
+from typing import Dict, Union
+
 from smtplib import SMTPException, SMTPAuthenticationError, SMTPSenderRefused
-from app.utils import EmailSender
-from app.schemas.email import SendEmailRequestBody
+import botocore.exceptions
+
+from app.utils import EmailSender, SESEmailSender
+from app.schemas.email import SendEmailRequestBody, EmailProvider
 
 
 class EmailService:
@@ -12,35 +16,48 @@ class EmailService:
     Service layer for handling email sending logic.
     """
 
-    def __init__(self, email_sender: EmailSender):
+    def __init__(
+        self, email_sender: Dict[EmailProvider, Union[EmailSender, SESEmailSender]]
+    ):
         self.email_sender = email_sender
 
     def send_email(self, email_details: SendEmailRequestBody, body: str):
         """
         Sends an email based on email details.
-
         Args:
             email_details (SendEmailRequestBody): Details for the email.
             body (str): body of the email.
 
         Returns:
-            bool: True if no failure occured on sending email.
+            dict: A response dictionary containing:
+                - "success" (bool): Indicates whether the email was sent successfully.
+                - "error": Error message if the email sending fails.
         """
         try:
-            result = self.email_sender.send_email(
-                to_email=email_details.recipient,
-                subject=email_details.subject,
-                body=body,
-                cc=email_details.cc,
-                bcc=email_details.bcc,
-                is_html=True,
-            )
+            result = self._send_email_by_provider(email_details, body)
             return result
-        except SMTPAuthenticationError as exc:
-            raise PermissionError(
-                "Failed to authenticate with the SMTP server."
-            ) from exc
-        except SMTPSenderRefused as exc:
-            raise ValueError("Sender address refused by the SMTP server.") from exc
-        except SMTPException as smtp_exc:
-            raise ConnectionError(f"SMTP error occurred: {smtp_exc}") from smtp_exc
+        except Exception as error:
+            self._handle_email_error(error)
+
+    def _send_email_by_provider(self, email_details: SendEmailRequestBody, body: str):
+        provider = email_details.provider
+        sender = self.email_sender.get(provider)
+        result = sender.send_email(
+            to_email=email_details.recipient,
+            subject=email_details.subject,
+            body=body,
+            cc=email_details.cc,
+            bcc=email_details.bcc,
+            is_html=True,
+        )
+        return result
+
+    def _handle_email_error(self, error: Exception):
+        if isinstance(
+            error, (SMTPAuthenticationError, botocore.exceptions.ClientError)
+        ):
+            raise PermissionError(f"Authentication failed: {str(error)}") from error
+        elif isinstance(error, SMTPSenderRefused):
+            raise ValueError(f"Email address refused: {str(error)}") from error
+        else:
+            raise ConnectionError(f"Email sending failed: {str(error)}") from error
--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -3,9 +3,11 @@ All the dependencies are defined here.
 """
 
 from functools import wraps
+from typing import Dict, Union
 from fastapi import HTTPException, Request
 from app.config import config
-from .email_sender import EmailSender
+from app.schemas.email import EmailProvider
+from .email_sender import EmailSender, SESEmailSender
 
 
 def require_authentication():
@@ -27,14 +29,23 @@ def require_authentication():
     return decorator
 
 
-def get_email_sender() -> EmailSender:
+def get_email_sender() -> Dict[EmailProvider, Union[EmailSender, SESEmailSender]]:
     """
-    Creates and returns an EmailSender object.
+    Creates and returns a dictionary of email sender objects.
     This function is used as a dependency injection in the controller.
     """
-    return EmailSender(
+    gmail_sender = EmailSender(
         smtp_server=config.EMAIL_HOST,
         smtp_port=config.EMAIL_PORT,
         username=config.EMAIL_ADDRESS,
         password=config.EMAIL_PASSWORD,
     )
+
+    ses_sender = SESEmailSender(
+        aws_access_key=config.AWS_ACCESS_KEY,
+        aws_secret_key=config.AWS_SECRET_KEY,
+        aws_region=config.AWS_REGION,
+        aws_email=config.AWS_EMAIL,
+    )
+
+    return {EmailProvider.GMAIL: gmail_sender, EmailProvider.SES: ses_sender}
