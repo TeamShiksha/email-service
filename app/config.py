d7f7655d0e8e6f5be70d88a2913b4b5780d429f6
Vaishnavi <gaikwadvaishnavi1000@gmail.com>
2026-06-01 23:47:07 +0530

    add interview feedback template

--- app/config.py
+++ app/config.py
@@ -39,6 +39,7 @@ TEMPLATE_HASH_MAP = {
     13: "swags.me/ForgotPassword.html",
     14: "swags.me/Verify.html",
     15: "teamshiksha/Interview.html",
+    16: "teamshiksha/InterviewFeedback.html",
 }
 
 
